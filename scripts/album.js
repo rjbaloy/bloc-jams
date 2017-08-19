7bac007b8fdb79407c4cf75580089160d0dc748a
Rich <rjbaloy@gmail.com>
2017-08-19 13:48:39 -1000

    checkpoint12 work

--- scripts/album.js
+++ scripts/album.js
@@ -49,7 +49,7 @@ var albumSingers = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -89,8 +89,27 @@ var setCurrentAlbum = function(album) {
 
 var albums = [albumPicasso, albumMarconi, albumSingers];
 var index = 1;
+// Elements we'll be adding listeners to
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+    songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+        // Selects first child element, which is the song-item-number element
+        this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+    }
+
     albumImage.addEventListener("click", function(event){
         setCurrentAlbum(albums[index]);
         index++;
